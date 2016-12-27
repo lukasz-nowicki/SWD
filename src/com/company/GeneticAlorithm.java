a4d8cb97914a95a83585c8bcf87a3b7049c2bb5b
lukasz-nowicki <lukasz_nowicki@hotmail.com>
2016-12-27 14:31:07 +0100

    Add knapsack and geneticalgorithm classes
    Add two classes.

---
+++ src/com/company/GeneticAlorithm.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by Newuser on 2016-12-27.
+ */
+
+// Główny algorytm
+public class GeneticAlorithm {
+
+}
---
+++ src/com/company/KnapsackProblem.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by Newuser on 2016-12-27.
+ */
+public class KnapsackProblem {
+}
